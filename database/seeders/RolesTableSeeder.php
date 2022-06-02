54efd457ed62592809d265e4b5d9556ec98569fd
celio.filho <celio.filho@agu.gov.br>
2022-06-02 13:55:41 -0300

    correcao da RolesTableSeeder

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -23,8 +23,7 @@ class RolesTableSeeder extends Seeder
         ]);
 
         UserRole::insert([
-            ['user_id'=>1,'role_id'=>2],
-            ['user_id'=>2,'role_id'=>3],
+            ['user_id'=>1,'role_id'=>2]
         ]);
     }
 }
